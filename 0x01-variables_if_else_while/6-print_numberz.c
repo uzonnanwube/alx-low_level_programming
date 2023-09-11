af9732865eb20e07f30b8e3d20f4958a0a39eb36
uzonnanwube <uzonnanwbe@gmail.com>
2023-09-11 05:40:07 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print alphabets without char and use putchar
+ *
+ * Return:  0 (Success)
+ *
+*/
+int main(void)
+{
+	int alphabets = '0';
+	
+	while (alphabets <= '9')
+	{
+		putchar (alphabets);
+		alphabets++;
+	}	
+	putchar ('\n');
+	return (0);
+}
