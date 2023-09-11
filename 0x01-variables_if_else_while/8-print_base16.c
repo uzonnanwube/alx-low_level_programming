ef87e31ceb11b53f488ee0a484716fe13ffcd57b
uzonnanwube <uzonnanwbe@gmail.com>
2023-09-11 08:12:27 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+* main - print hexadecimal numbers
+*
+* Return only 0 (success)
+*
+*/
+int main(void)
+{
+	char numbers = '0';
+	char letters = 'a';
+
+	while (numbers <= '9')
+	{
+		putchar (numbers);
+		numbers++;
+	}
+	while (letters <= 'f')
+	{
+		putchar (letters);
+		letters++;
+	}
+	putchar('\n');
+	return (0);
+}	
+
+	
+	
