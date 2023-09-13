0790fb2138612d17e50934b4d9bcb22f8ebf922f
uzonnanwube <uzonnanwbe@gmail.com>
2023-09-13 09:45:57 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,14 +6,15 @@
  *
  */
 int main(void)
-{	
-	char base_ten ='0';
-	
-	while (base_ten <='9')
+{
+	char base_ten = '0';
+
+	while (base_ten <= '9')
 	{
 		putchar(base_ten);
 		base_ten++;
 	}
 	putchar('\n');
+
 	return (0);
-} 	
+}
