de1c48f2105eb836da7b1aac9467a103088f5568
uzonnanwube <uzonnanwbe@gmail.com>
2023-09-14 00:59:52 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-1"
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
-}	
+}
