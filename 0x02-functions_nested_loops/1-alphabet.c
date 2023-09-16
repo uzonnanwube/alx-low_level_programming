4ce36a6f2c8dc4e1c22c4326ab4ff31339060100
uzonnanwube <uzonnanwbe@gmail.com>
2023-09-16 07:21:25 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_alphabet - print all alphabets in lowercases
  */
@@ -8,7 +7,10 @@ void print_alphabet(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-	       _putchar(letter);
+	{
+		_putchar(letter);
+	}
 
 	_putchar('\n');
+
 }
