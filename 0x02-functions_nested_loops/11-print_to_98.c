89e42b5bd1ad9ed1969d963c820a432ed83fd75c
uzonnanwube <uzonnanwbe@gmail.com>
2023-09-19 05:28:52 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,27 +6,27 @@
  * followed by a new line
  * @n: print from this number
  */
-void print_to_98 (int n)
+void print_to_98(int n)
 {
-	int i, j;
+	int i;
 
-	if (n <= )
+	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++;)
+		for (i = n; i <= 98; i++)
 		{
+			printf("%d", i);
 			if (i != 98)
-				printf("%d");
-			else if (i == 98)
-				prinf("%d\n", i);
-		}
-	}else if (n >= 98)
-	{
-		for (j = n; j >= 98; j--)
-		{
-			if(j != 98)
-				printf("%d, ", j);
-			else if (j == 98)
-				printf ("%d\n", j);
+				printf(",");
 		}
 	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(" ");
+		}
+	}
+	printf("\n");
 }
